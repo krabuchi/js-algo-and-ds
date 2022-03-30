f4e749ce3f457702d70885d0931dce5cae4e5ec9
krabuchi <shashu95@yahoo.com>
2022-03-30 21:56:26 +0530

    singly-linked-list: added shift, unshift, get and set

--- Data_Structures/singly_linked_list.js
+++ Data_Structures/singly_linked_list.js
@@ -14,7 +14,7 @@ class SinglyLinkedList {
 
     push(val) {
         var newNode = new Node(val);
-        if(!this.head) {
+        if (!this.head) {
             this.head = newNode;
             this.tail = this.head;
         } else {
@@ -26,20 +26,67 @@ class SinglyLinkedList {
     }
 
     pop() {
-        if(!this.head) return undefined;
+        if (!this.head) return undefined;
         var current = this.head;
         var newTail = current;
-        while(current.next !== null) {
+        while (current.next !== null) {
             newTail = current;
             current = current.next;
         }
         this.tail = newTail;
         this.tail.next = null;
         this.length--;
-        if(this.length ===0) {
+        if (this.length === 0) {
             this.head = null;
             this.tail = null;
         }
         return current;
     }
+
+    shift() {
+        if (!this.head) return undefined;
+        var temp = this.head;
+        this.head = temp.next;
+        this.length--;
+        if (this.length === 0) this.tail = null;
+        return temp;
+    }
+
+    unshift(val) {
+        var newNode = new Node(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    get(idx) {
+        if (idx <= 0 || idx >= this.length) return null;
+        var limit = idx;
+        var counter = 0;
+        var firstItem = this.head;
+        while (counter < limit) {
+            firstItem = firstItem.next;
+            counter++;
+        }
+        return firstItem;
+    }
+
+    set(idx, val) {
+        var cur = this.get(idx);
+        if(!cur) return false;
+        cur.value = val;
+        return true;
+    }
 }
+
+var list = new SinglyLinkedList();
+list.push("1");
+list.push("2");
+list.push("3");
+list.push("4");
