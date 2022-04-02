3a71664ce8b9eef002225d04741c648cf190c06d
krabuchi <shashu95@yahoo.com>
2022-04-02 19:48:30 +0530

    DLL methods: get, set, insert, remove and reverse

--- Data_Structures/doubly_linked_list.js
+++ Data_Structures/doubly_linked_list.js
@@ -7,7 +7,7 @@ class Node {
 }
 
 class DoublyLinkedList {
-    constructor () {
+    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
@@ -15,7 +15,7 @@ class DoublyLinkedList {
 
     push(val) {
         var newNode = new Node(val);
-        if(!this.head) {
+        if (!this.head) {
             this.head = newNode;
             this.tail = this.head;
         } else {
@@ -29,9 +29,9 @@ class DoublyLinkedList {
     }
 
     pop() {
-        if(!this.head) return undefined;
+        if (!this.head) return undefined;
         var curTail = this.tail;
-        if(this.length === 1) {
+        if (this.length === 1) {
             this.head = null;
             this.tail = null;
         } else {
@@ -44,9 +44,9 @@ class DoublyLinkedList {
     }
 
     shift() {
-        if(!this.head) return undefined;
+        if (!this.head) return undefined;
         var oldHead = this.head;
-        if(this.length === 1) {
+        if (this.length === 1) {
             this.head = null;
             this.tail = null;
         } else {
@@ -60,7 +60,7 @@ class DoublyLinkedList {
 
     unshift(val) {
         var newNode = new Node(val);
-        if(!this.head) {
+        if (!this.head) {
             this.head = newNode;
             this.tail = newNode;
         } else {
@@ -70,6 +70,79 @@ class DoublyLinkedList {
             this.head = newNode;
         }
         this.length++;
+        return this;
+    }
+
+    get(idx) {
+        if (idx < 0 || idx > this.length) return null;
+        if (idx <= this.length / 2) {
+            var counter = 0;
+            var firstItem = this.head;
+            while (counter !== idx) {
+                firstItem = firstItem.next;
+                counter++;
+            }
+            return firstItem;
+        } else {
+            var counter = this.length - 1;
+            var lastItem = this.tail;
+            while (counter !== idx) {
+                lastItem = lastItem.prev;
+                counter--;
+            }
+            return lastItem;
+        }
+    }
+
+    set(idx, val) {
+        var t = this.get(idx);
+        if (t !== null) {
+            t.value = val;
+            return true;
+        }
+        return false;
+    }
+
+    insert(idx, val) {
+        if(idx < 0 || idx > this.length) return false;
+        if(idx === 0) return this.unshift(val);
+        if(idx === this.length) return this.push(val);
+        var newNode = new Node(val);
+        var before = this.get(idx - 1);
+        var after = before.next; 
+        before.next = newNode;
+        newNode.prev = before;
+        newNode.next = after;
+        after.prev = newNode;
+        this.length++;
+        return true;
+    }
+
+    remove(idx) {
+        if(idx < 0 || idx >= this.length) return undefined;
+        if(idx === 0) return this.shift();
+        if(idx === this.length - 1) return this.pop();
+        var item = this.get(idx);
+        var prev = item.prev;
+        var next = item.next;
+        prev.next = next;
+        next.prev = prev;
+        item.next = null, item.prev = null;
+        this.length--;
+        return item;
+    }
+
+    reverse() {
+        var node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+        for(var i = 0; i < this.length; i++) { 
+            var k = node.next;
+            var temp = k;
+            node.next = node.prev;
+            node.prev = k;
+            node = temp;
+        }
         return this;
     }
 }
