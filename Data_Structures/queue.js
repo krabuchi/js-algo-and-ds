7d6444ce848ee9847e9af3e0b10606253e4c8301
krabuchi <shashu95@yahoo.com>
2022-04-04 21:56:45 +0530

    DS: Stack and Queue

--- Data_Structures/queue.js
+++ Data_Structures/queue.js
@@ -1,6 +1,6 @@
-/* 
-Stack(collection of data) is abstract DS
-Stack is a LIFO(Last In First Out) DS
+/*
+    Abstract DS to add and remove data 
+    First In First Out
 */
 
 class Node {
@@ -10,36 +10,36 @@ class Node {
     }
 }
 
-class Stack {
+class Queue {
     constructor() {
         this.first = null;
         this.last = null;
         this.size = 0;
     }
 
-    push(val) {
+    enqueue(val) {
         var newNode = new Node(val);
         if(!this.first) {
             this.first = newNode;
-            this.last = newNode;
+            this.last = this.first;
         } else {
-            var first = this.first;
-            this.first = newNode;
-            this.first.next = first;
+            this.last.next = newNode;
+            this.last = newNode;
         }
         return this.size++;
     }
 
-    pop() {
+    dequeue() {
         if(!this.first) return null;
         var first = this.first;
-        if(this.size === 1) {
+        if(first === this.last) {
             this.first = null;
             this.last = null;
         } else {
             this.first = first.next;
+            first.next = null;
         }
         this.size--;
-        return first;
+        return first; 
     }
 }
