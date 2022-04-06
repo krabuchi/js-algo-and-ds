84c62589f9856e94e9677edeb8ef0cd240e5fbbf
krabuchi <shashu95@yahoo.com>
2022-04-06 21:55:31 +0530

    DS: binary search tree insert & find

--- Data_Structures/trees.js
+++ Data_Structures/trees.js
@@ -20,10 +20,56 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    insert(val) {
+        var newNode = new Node(val);
+        if(!this.root) {
+            this.root = newNode;
+            return this;
+        } else {
+            var current = this.root;
+            while(true) {
+                if(val < current.value) {
+                    if(current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                }  else if(val > current.value) {
+                    if(current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                } else {
+                    return undefined;
+                }
+            }
+        }
+    }
+
+    find(val) {
+        if(this.root === null) return false;
+        var current = this.root;
+        var found = false;
+        while(current && !found) {
+            if(val < current.value) {
+                current = current.left;
+            } else if (val > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        if(!found) return undefined;
+        return false;
+    }
 }
 
 var tree = new BinarySearchTree();
-tree.root = new Node(10);
-tree.root.right = new Node(15);
-tree.root.left = new Node(7);
-tree.root.left.right = new Node(9);
+tree.insert(10);
+tree.insert(18);
+tree.insert(5);
+tree.insert(6);
