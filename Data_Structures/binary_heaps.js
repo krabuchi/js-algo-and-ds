11f7e202a788e5f7c15015b6a3ab990bb78efc97
krabuchi <shashu95@yahoo.com>
2022-04-11 21:36:35 +0530

    binary heaps extractMax

--- Data_Structures/binary_heaps.js
+++ Data_Structures/binary_heaps.js
@@ -20,6 +20,46 @@ class MaxBinaryHeap {
         this.values.push(element);
         this.bubbleUp();
     }
+
+    sinkDown() {
+        var idx = 0;
+        var length = this.values.length;
+        var element = this.values[0];
+        while (true) {
+            var leftChildIdx = 2 * idx + 1;
+            var rightChildIdx = 2 * idx + 2;
+            var leftChild, rightChild;
+            var swap = null;
+            if (leftChildIdx < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild > element) {
+                    swap = leftChildIdx;
+                }
+            }
+            if (rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (swap === null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+            if (swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+        }
+    }
+
+    extractMax() {
+        var first = this.values[0];
+        var last = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = last;
+            this.sinkDown();
+        }
+        return first;
+    }
 }
 
 
