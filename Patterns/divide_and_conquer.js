9daecfd2f4a351e22b57139483d4030a2ddd2117
krabuchi <shashu95@yahoo.com>
2022-09-11 11:51:46 +0530

    pattern divide and conquer search binary

--- Patterns/divide_and_conquer.js
+++ Patterns/divide_and_conquer.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+/* 
+This pattern invokes dividing a data set into smaller chunks and then repeating process with subset of 
+data. This pattern can tremendously reduce time complexity.   
+*/
+
+/*
+ Given sorted array of integers, write a function called search, that accepts a value and returns the index where
+  the value passed to function is located. if the value is not found, return -1.
+*/
+
+const search = (arr, val) => {
+    let min = 0;
+    let max = arr.length - 1;
+    while(min <= max) {
+        let midpoint = Math.floor((min + max) / 2);
+        if(arr[midpoint] > val) {            
+            max = midpoint - 1;
+        } else if (arr[midpoint] > val) {
+            min = midpoint + 1;
+        } else {
+            return midpoint;
+        }
+    }
+    return -1;
+}
